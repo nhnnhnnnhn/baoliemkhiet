5bf22026aae131190a8bf3a67a52913d4703b13c
Kien <trungkienpham0607@gmail.com>
2025-03-09 01:35:37 +0700

    Feat: Update title

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+// src/App.js
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/common/header";
 import MainTitle from "./components/common/MainTitle";
+
+// Import trang sport
+import SportPage from "./pages/news/sport"; 
+
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
-      <MainTitle/>
-    </div>
+      <Routes>
+        <Route path="/" element={<MainTitle />} />
+        {/* Trang sport */}
+        <Route path="/sport" element={<SportPage />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/common/header/index.jsx
+++ src/components/common/header/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { AppBar, Toolbar, Button, IconButton, InputBase, Box } from "@mui/material";
 import { Search, Notifications } from "@mui/icons-material";
 import MenuIcon from "@mui/icons-material/Menu";
+import { Link } from "react-router-dom";  // Import Link từ react-router-dom
 import "./Header.css";
 import logo from "../../../assets/logo.png";
 
@@ -9,18 +10,30 @@ const Header = () => {
   return (
     <AppBar position="static" className="header">
       <Toolbar>
-        {/* Logo */}
+        {/* Logo - bấm vào để về trang chủ */}
         <Box className="logo-container">
-          <img src={logo} alt="Logo" className="logo" />
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <img src={logo} alt="Logo" className="logo" />
+          </Link>
         </Box>
 
         {/* Danh mục */}
         <Box className="nav-links">
-          <Button className="custom-nav-link">Thời sự</Button>
-          <Button className="custom-nav-link">Thế giới</Button>
-          <Button className="custom-nav-link">Kinh doanh</Button>
-          <Button className="custom-nav-link">Công nghệ</Button>
-          <Button className="custom-nav-link">Thể thao</Button>
+          <Button component={Link} to="/news/thoi-su" className="custom-nav-link">
+            Thời sự
+          </Button>
+          <Button component={Link} to="/news/the-gioi" className="custom-nav-link">
+            Thế giới
+          </Button>
+          <Button component={Link} to="/news/kinh-doanh" className="custom-nav-link">
+            Kinh doanh
+          </Button>
+          <Button component={Link} to="/news/cong-nghe" className="custom-nav-link">
+            Công nghệ
+          </Button>
+          <Button component={Link} to="/sport" className="custom-nav-link">
+            Thể thao
+          </Button>
         </Box>
 
         {/* Thanh tìm kiếm */}
@@ -44,14 +57,14 @@ const Header = () => {
       </Toolbar>
 
       {/* Dòng chữ chạy breaking news */}
-      <Box className="breaking-news" sx={{ width: '100%' }}>
-      <div className="news-content">
-        <span>ĐỘI BÓNG HÀNG DA ĐÁNH RƠI CHIẾN THẮNG TRƯỚC ANH LONG</span>
-        <span>NÓNG: PHẠM THOẠI TUNG 28 TRANG SAO KÊ TÀI KHOẢN MB</span>
-        <span>KHUYẾN MÃI LỚN DỊP 8/3 - ƯU ĐÃI KHỦNG!</span>
-      </div>
+      <Box className="breaking-news" sx={{ width: "100%" }}>
+        <div className="news-content">
+          <span>ĐỘI BÓNG HÀNG DA ĐÁNH RƠI CHIẾN THẮNG TRƯỚC ANH LONG</span>
+          <span>NÓNG: PHẠM THOẠI TUNG 28 TRANG SAO KÊ TÀI KHOẢN MB</span>
+          <span>KHUYẾN MÃI LỚN DỊP 8/3 - ƯU ĐÃI KHỦNG!</span>
+        </div>
       </Box>
-      </AppBar>
+    </AppBar>
   );
 };
 
