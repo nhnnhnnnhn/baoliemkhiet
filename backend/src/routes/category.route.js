410bfbca3f3d986df1eb317d02a09c84c64761aa
nhaatj <nguyenngocnhat2004st@gmail.com>
2025-04-14 20:52:54 +0700

    add auth

--- backend/src/routes/category.route.js
+++ backend/src/routes/category.route.js
@@ -1,28 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const controller = require('../controllers/category.controller');
-const authMiddleware = require('../middlewares/auth.middleware');
+const controller = require("../controllers/category.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
 
 // Create a new category
-router.post('/create', controller.createCategory);
+router.post("/create", authMiddleware, controller.createCategory);
 
 // Get all categories
-router.get('/get', controller.getAllCategories);
+router.get("/get", authMiddleware, controller.getAllCategories);
 
 // Get a single category by ID
-router.get('/get/:id', controller.getCategoryById);
+router.get("/get/:id", authMiddleware, controller.getCategoryById);
 
 // Edit a category
-router.patch('/edit/:id', controller.editCategory);
+router.patch("/edit/:id", authMiddleware, controller.editCategory);
 
 // Delete a category
-router.delete('/delete/:id', controller.deleteCategory);
+router.delete("/delete/:id", authMiddleware, controller.deleteCategory);
 
 // Delete multiple categories
-router.delete('/delete-multiple', controller.deleteMultipleCategories);
+router.delete(
+  "/delete-multiple",
+  authMiddleware,
+  controller.deleteMultipleCategories
+);
 
 // Get articles count by category
-router.get('/articles-count', controller.getArticlesCountByCategory);
+router.get(
+  "/articles-count",
+  authMiddleware,
+  controller.getArticlesCountByCategory
+);
 
 module.exports = router;
